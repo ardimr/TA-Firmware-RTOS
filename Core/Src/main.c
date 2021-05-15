a7e9afb9e329840b6f65272a8170728c499d2336
ardimr <rizkyardimaulana@gmail.com>
2021-05-15 09:58:59 +0700

    Fixing GPS Compiling error
    Fixing GPS Compiling error

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -29,6 +29,7 @@
 #include "mpu6050.h"
 #include "MFRC522.h"
 #include "integer_type.h"
+#include <math.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -50,7 +51,9 @@ I2C_HandleTypeDef hi2c1;
 
 SPI_HandleTypeDef hspi1;
 
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart1_rx;
 
 /* Definitions for DisplayTask */
 osThreadId_t DisplayTaskHandle;
@@ -103,14 +106,21 @@ MPU6050_t MPU6050;
 //RFID Variable
 uint8_t UID[4]={};
 
+//GPS Variable
+float latitude = 0;
+float longitude = 0;
+char strUTC[8] = {}; // UTC time in the readable hh:mm:ss format
+uint8_t flag = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_SPI1_Init(void);
+static void MX_USART1_UART_Init(void);
 void Display(void *argument);
 void IMU(void *argument);
 void GPS(void *argument);
@@ -119,7 +129,17 @@ void RFID(void *argument);
 void SDCard(void *argument);
 
 /* USER CODE BEGIN PFP */
-
+// function to calculate checksum of the NMEA sentence
+// -4, but not -3 because the NMEA sentences are delimited with \r\n, and there also is the invisible \r in the end
+int nmea0183_checksum(char *msg) {
+	int checksum = 0;
+	int j = 0;
+	// the first $ sign and the last two bytes of original CRC + the * sign
+	for (j = 1; j < strlen(msg) - 4; j++) {
+		checksum = checksum ^ (unsigned) msg[j];
+	}
+	return checksum;
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -155,9 +175,11 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART2_UART_Init();
   MX_I2C1_Init();
   MX_SPI1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   char txBuffer [100] = {};
   sprintf(txBuffer, "Bismillah..\n");
@@ -191,16 +213,16 @@ int main(void)
   IMUTaskHandle = osThreadNew(IMU, NULL, &IMUTask_attributes);
 
   /* creation of GPSTask */
-//  GPSTaskHandle = osThreadNew(GPS, NULL, &GPSTask_attributes);
+  GPSTaskHandle = osThreadNew(GPS, NULL, &GPSTask_attributes);
 
   /* creation of AnalogReadingTa */
-//  AnalogReadingTaHandle = osThreadNew(AnalogReading, NULL, &AnalogReadingTa_attributes);
+  AnalogReadingTaHandle = osThreadNew(AnalogReading, NULL, &AnalogReadingTa_attributes);
 
   /* creation of RFIDTask */
   RFIDTaskHandle = osThreadNew(RFID, NULL, &RFIDTask_attributes);
 
   /* creation of SDCardTask */
-//  SDCardTaskHandle = osThreadNew(SDCard, NULL, &SDCardTask_attributes);
+  SDCardTaskHandle = osThreadNew(SDCard, NULL, &SDCardTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -345,6 +367,39 @@ static void MX_SPI1_Init(void)
 }
 
 /**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -374,6 +429,22 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream2_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream2_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream2_IRQn);
 
 }
 
@@ -489,10 +560,180 @@ void IMU(void *argument)
 void GPS(void *argument)
 {
   /* USER CODE BEGIN GPS */
+	uint8_t buff[255];
+	  char buffStr[255];
+	  char nmeaSnt[80];
+
+	  char *rawSum;
+	  char smNmbr[3];
+
+	  // The Equator has a latitude of 0°,
+	  //the North Pole has a latitude of 90° North (written 90° N or +90°),
+	  //and the South Pole has a latitude of 90° South (written 90° S or −90°)
+	  char *latRaw;
+	  char latDg[10];
+	  char latMS[20];
+	  char *hemNS;
+
+	  // longitude in degrees (0° at the Prime Meridian to +180° eastward and −180° westward)
+	  // that is why 3
+	  char *lonRaw;
+	  char lonDg[10];
+	  char lonMS[10];
+	  char *hemEW;
+
+	  char *utcRaw; // raw UTC time from the NMEA sentence in the hhmmss format
+
+
+	  char hH[2]; // hours
+	  char mM[2]; // minutes
+	  char sS[2]; // seconds
+
+	  uint8_t cnt = 0;
+
+	  HAL_UART_Receive_DMA(&huart1, buff, 255);
+
+	  HAL_UART_Transmit(&huart2, (unsigned char *) "Start\n", 6, 500);
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+	  char txBuffer[200] = {};
+	  sprintf(txBuffer,"\nFlag : %d", flag);
+	  //	  HAL_UART_Transmit(&huart2, (uint8_t*) txBuffer, sizeof(txBuffer), 100);
+	  if (flag) {
+	  	memset(buffStr, 0, 255);
+	  	sprintf(buffStr, "%s", buff);
+	  //HAL_UART_Transmit(&huart2, (uint8_t *)buffStr, sizeof(buffStr), 70);
+	  // splitting the buffStr by the "\n" delimiter with the strsep() C function
+	  // see http://www.manpagez.com/man/3/strsep/
+	  	char *token, *string;
+	  	// actually splitting the string by "\n" delimiter
+	  	string = strdup(buffStr);
+	  	while ((token = strsep(&string, "\n")) != NULL) {
+	  		memset(nmeaSnt, 0, 80);
+	  		sprintf(nmeaSnt, "%s", token);
+
+	  		memset(txBuffer,0,sizeof(txBuffer));
+	  //HAL_UART_Transmit(&huart2, (unsigned char*) txBuffer, sizeof(txBuffer), 100);
+	  // selecting only $GNGLL sentences, combined GPS and GLONAS
+	  // on my GPS sensor this good NMEA sentence is always 50 characters
+	  		if ((strstr(nmeaSnt, "$GPGGA") != 0) && (strlen(nmeaSnt) > 49) &&(strlen(nmeaSnt) <90) && strstr(nmeaSnt, "*") != 0) {
+	  			rawSum = strstr(nmeaSnt, "*");
+	  			memcpy(smNmbr, &rawSum[1], 2);
+	  			smNmbr[2] = '\0';
+
+	  			uint8_t intSum = nmea0183_checksum(nmeaSnt);
+	  			char hex[2];
+	  			// "%X" unsigned hexadecimal integer (capital letters)
+	  			sprintf(hex, "%X", intSum);
+
+	  			// checksum data verification, if OK, then we can really trust
+	  			// the data in the the NMEA sentence
+	  			if (strstr(smNmbr, hex) != NULL) {
+	  				cnt = 0;
+	  //			sprintf(txBuffer,"pV : %s\n", pV);
+	  			// splitting the good NMEA sentence into the tokens by the comma delimiter
+	  				for (char *pV = strtok(nmeaSnt, ","); pV != NULL; pV = strtok(NULL, ",")) {
+	  					memset(txBuffer,0,sizeof(txBuffer));
+	  					sprintf(txBuffer,"pV[%d] : %s\n",cnt, pV);
+	  //				HAL_UART_Transmit(&huart2, (unsigned char *) txBuffer, sizeof(txBuffer), 100);
+	  					switch (cnt) {
+	  						case 1:
+	  							  utcRaw = strdup(pV);
+	  							  break;
+	  						case 2:
+	  							  latRaw = strdup(pV);
+	  							  break;
+	  						case 3:
+	  							  hemNS = strdup(pV);
+	  							  break;
+	  						case 4:
+	  							  lonRaw = strdup(pV);
+	  							  break;
+	  						case 5:
+	  							  hemEW = strdup(pV);
+	  							  break;
+	  					}
+	  					cnt++;
+	  				} //end for
+
+	  				//Converting Longitude and Latitude into Float
+	  				latitude = atof(latRaw);
+	  				longitude = atof(lonRaw);
+
+	  				if (*hemNS == 'S') {
+	  					latitude  *= -1.0;
+	  				}
+	  				if (*hemEW == 'W') {
+	  					longitude *= -1.0;
+	  				}
+	  				char * token;
+	  				//Get LatitudeDegree
+	  				token = strtok(latRaw, ".");
+	  				memset(latDg, 0, sizeof(latDg));
+	  //			memcpy(latDg, token, strlen(token));
+	  				sprintf(latDg, token);
+	  				//Get Minutes
+	  				token = strtok(NULL,".");
+	  				memset(latMS, 0, sizeof(latMS));
+	  //			memcpy(latMS, token, strlen(token));
+	  				sprintf(latMS, token);
+	  //			latMS[7] = '.';
+
+	  				//Get longitude Degree
+	  				float degrees = trunc(latitude / 100.0f);
+	  				float minutes = latitude - (degrees * 100.0f);
+	  				latitude = degrees + (minutes / 60.0f);
+
+	  				degrees = trunc(longitude / 100.0f);
+	  				minutes = longitude - (degrees * 100.0f);
+	  				longitude = degrees + (minutes / 60.0f);
+
+
+	  				token = strtok(lonRaw, ".");
+	  				memset(lonDg, 0, sizeof(lonDg));
+	  				memcpy(lonDg, token, strlen(token));
+
+	  				token = strtok(NULL, ".");
+	  				memset(lonMS, 0, sizeof(lonMS));
+	  				memcpy(lonMS, token, strlen(token));
+
+	  				memset(txBuffer,0,sizeof(txBuffer));
+	  				sprintf(txBuffer, "latDg : %s latMs : %s lonDg : %s lonMs : %s\n",latDg,latMS,lonDg,lonMS );
+	  //		  HAL_UART_Transmit(&huart2, (unsigned char *)txBuffer, sizeof(txBuffer), 100);
+
+	  					  //converting the UTC time in the hh:mm:ss format
+	  				memcpy(hH, &utcRaw[0], 2);
+	  				hH[2] = '\0';
+
+	  				memcpy(mM, &utcRaw[2], 2);
+	  				mM[2] = '\0';
+
+	  				memcpy(sS, &utcRaw[4], 2);
+	  				sS[2] = '\0';
+
+//	  				float latDg_f = atof(latDg)/100.0;
+//	  				float latMS_f = atof(latMS)/60.0;
+//	  				float lonDg_f = atof(lonDg)/100.0;
+//	  				float lonMS_f = atof(lonMS)/60.0;
+
+	  				strcpy(strUTC, hH);
+	  				strcat(strUTC, ":");
+	  				strcat(strUTC, mM);
+	  				strcat(strUTC, ":");
+	  				strcat(strUTC, sS);
+	  				strUTC[8] = '\0';
+
+	  				memset(txBuffer,0,sizeof(txBuffer));
+	  				sprintf(txBuffer, "Latitude : %f Longitude : %f UTC : %s\n",latitude,longitude, strUTC);
+	  				HAL_UART_Transmit(&huart2, (unsigned char *)txBuffer, sizeof(txBuffer), 100);
+
+	  			} //end of the chekcsum data verification
+	  		} //end of %GPPGA Sentences selection
+	  	}// end of splotting the buffstr by the "\n" delimiter with strsep() c function
+	  	flag = 0;
+	  }
+	  osDelay(500);
   }
   /* USER CODE END GPS */
 }
